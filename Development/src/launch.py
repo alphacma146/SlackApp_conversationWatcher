bd5fa4e1b410ec719ad265d57bf39a16ef4b2442
syotaro <m154030kdy@gmail.com>
2023-01-06 01:18:48 +0900

    view layout

--- Development/src/launch.py
+++ Development/src/launch.py
@@ -1,6 +1,28 @@
+# Standard lib
+import sys
+from pathlib import Path
+# Self made
+from view import View
+
 
 def main():
-    pass
+    root_path = get_root()
+    version = get_version()
+    View(root_path=root_path, version=version).run()
+
+
+def get_version() -> str:
+
+    return "Ver. 0.9.0.0"
+
+
+def get_root() -> Path:
+    if getattr(sys, "frozen", False):
+        ret = Path(sys._MEIPASS)
+    else:
+        ret = Path(__file__).parent
+
+    return ret
 
 
 if __name__ == "__main__":
---
+++ Development/src/view.py
@@ -0,0 +1,38 @@
+# Third party
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+from kivy.core.text import LabelBase, DEFAULT_FONT
+from kivy.resources import resource_add_path
+from kivy.config import Config
+from kivy.lang import Builder
+
+
+class RootWidget(BoxLayout):
+
+    ver_text = StringProperty()
+
+    def __init__(self, version):
+        super().__init__()
+        self.ver_text = version.rsplit(".", 1)[0]
+
+    # ボタンをクリック時
+    def on_command(self, **kwargs):
+        # self.ver_text = self.ids.text1.text
+        pass
+
+
+class View(App):
+    def __init__(self, root_path, version):
+        super().__init__()
+        Builder.load_file(str(root_path / "view_layout.kv"))
+        Config.set('graphics', 'fullscreen', '0')
+        Config.set('graphics', 'width', '470')
+        Config.set('graphics', 'height', '320')
+        resource_add_path('c:/Windows/Fonts')
+        LabelBase.register(DEFAULT_FONT, 'msgothic.ttc')
+        self.__version = version
+        self.title = "SlackLogAccumulator"
+
+    def build(self):
+        return RootWidget(self.__version)
