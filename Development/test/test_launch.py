28f49f535ceec912fa2713ab02868e21a2e2aa8d
syotaro <m154030kdy@gmail.com>
2023-02-05 21:26:07 +0900

    unittest

--- Development/test/test_launch.py
+++ Development/test/test_launch.py
@@ -1,26 +1,37 @@
-
+import pytest
+import sys
 from unittest import mock
 from pathlib import Path
-import pytest
 from src.launch import (
-    main,
     get_version,
     get_root,
     get_exe_directory
 )
 
 
-# @pytest.mark.asyncio
-# def test_main():
-#    assert main() is None
-
 def test_get_version():
-    assert get_version() == 'Ver. 0.9.0.0'
+    assert isinstance(get_version(), str)
 
 
-# def test_get_root():
-#    assert get_root() is Path
-#
-#
-# def test_get_exe_directory():
-#    assert get_exe_directory() is Path
+@pytest.mark.parametrize("isexist", [
+    True,
+    False
+])
+def test_get_root(isexist):
+    setattr(sys, "frozen", None)
+    setattr(sys, "_MEIPASS", None)
+    with (
+        mock.patch.object(sys, "frozen", isexist),
+        mock.patch.object(sys, "_MEIPASS")
+    ):
+        assert isinstance(get_root(), Path)
+
+
+@pytest.mark.parametrize("isexist", [
+    True,
+    False
+])
+def test_get_exe_directory(isexist):
+    setattr(sys, "frozen", None)
+    with mock.patch.object(sys, "frozen", isexist):
+        assert isinstance(get_exe_directory(), Path)
